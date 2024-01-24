e1f739d6fb8988433b5e8187691face3e3bb8928
SantiagoPN <sprada71@misena.edu.co>
2024-01-23 23:40:53 -0500

    ValidacionesNew

--- js/app.js
+++ js/app.js
@@ -1,50 +1,50 @@
 
-// Variables
-const user = document.querySelector('#usuario');
-const passwd = document.querySelector('#contrasena');
-const deposito = document.querySelector('#deposito');
-const multimedia = document.querySelector('#multimedia');
-const recaptchaResponse = () => grecaptcha.getResponse();
-const icon = document.querySelector('#textMulti');
-const btnSubmit = document.querySelector('#formulario button[type="submit"]');
-const formulario = document.querySelector('#formulario');
-const spinner = document.querySelector('#spinner');
 
-//Objeto que guardará los datos y los convierto a json
-const datosMultimedia = {
-    usuario: '',
-    contrasena: '',
-    deposit: '',
-    multi: '',
-    // captcha: ''
-};
-
-// console.log(recaptchaResponse);
-
-// Eventos 
+// DOM 
 document.addEventListener('DOMContentLoaded', e => {
+    // Variables
+    const user = document.querySelector('#usuario');
+    const passwd = document.querySelector('#contrasena');
+    const deposito = document.querySelector('#deposito');
+    const multimedia = document.querySelector('#multimedia');
+    const recaptchaResponse = () => grecaptcha.getResponse();
+    const captchaDiv = document.querySelector('.g-recaptcha');
+    const icon = document.querySelector('#textMulti');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const formulario = document.querySelector('#formulario');
+    const spinner = document.querySelector('#spinner');
+
+
+    //Objeto que guardará los datos y los convierto a json
+    const datosMultimedia = {
+
+    };
+
     e.preventDefault();
-    // Validamos si el objeto está lleno o vacio
-    const validarObjeto = () => {
-        const datos = Object.values(datosMultimedia);
-        const camposLlenos = datos.every(dato => dato !== '' && dato !== undefined && dato !== null);
-        if (camposLlenos) {
-            btnSubmit.disabled = false;
-            btnSubmit.classList.remove('opacity-50');
-        } else {
-            btnSubmit.disabled = true;
-            btnSubmit.classList.add('opacity-50');
-        }
+
+    const validacionForm = {
+        user: false,
+        pass: false,
+        deposit: false,
+        multi: false,
+        captcha: false
     }
+
+    const validCaptcha = () => {
+        console.log('validado pa');
+        validacionForm.captcha = true;
+    }
+
+    const captchaOption = {
+        key: '6LcC2a8nAAAAAFDgI8YEFCOvfkYKk7Og0WQ8fuj-',
+        callback: validCaptcha
+    }
+
+    const captchaComp = () => grecaptcha.render(captchaDiv, captchaOption);
+    console.log(captchaComp);
 
     // Limpiamos el formulario y reiniciamos el objeto
     const limpiarForm = () => {
-        //Reiniciar el objeto
-        datosMultimedia.usuario = '',
-            datosMultimedia.contrasena = '',
-            datosMultimedia.deposit = '',
-            datosMultimedia.multi = '';
-
         // Resetear el form 
         formulario.reset();
         // Resetear el valor del input
@@ -52,7 +52,8 @@ document.addEventListener('DOMContentLoaded', e => {
         icon.textContent = 'Subir archivos';
     }
 
-    // Validamos los campos de usuario y contraseña
+
+    // // Validamos los campos de usuario y contraseña
     const validarCampos = e => {
         if (e.target.value.trim() === '') {
             Swal.fire({
@@ -61,23 +62,23 @@ document.addEventListener('DOMContentLoaded', e => {
                 text: `¡El campo de ${e.target.id} es obligatorio!`,
                 footer: '<a href="https://soporte.unidrogas.co/zoho/" target="_blank">¿Tienes un problema?</a>'
             });
-            // Reseteamos el valor del objeto
-            datosMultimedia[e.target.name] = '';
-            // Validamos el objeto
-            validarObjeto();
             return;
         }
         //Asignar los valores al objeto
         datosMultimedia[e.target.name] = e.target.value.trim().toLowerCase();
-        validarObjeto();
+        console.log(validacionForm);
     }
+
+
+    // Agregamos la función de validar
+    user.addEventListener('blur', validarCampos);
+    passwd.addEventListener('blur', validarCampos, validacionForm.pass = true);
 
     //Envio del formulario 
     const envioFormulario = e => {
         e.preventDefault();
         spinner.classList.remove('hidden');
         spinner.classList.add('flex');
-
         const recaptchaValue = recaptchaResponse();
         if (!recaptchaValue) {
             console.log('No has llenado el recapcha mamaguevo');
@@ -89,7 +90,8 @@ document.addEventListener('DOMContentLoaded', e => {
             });
             return;
         }
-
+        // Asigancion del valor
+        datosMultimedia.captcha = 'Este usuario verificó el captcha';
         setTimeout(() => {
             spinner.classList.add('hidden');
             spinner.classList.remove('flex');
@@ -104,10 +106,6 @@ document.addEventListener('DOMContentLoaded', e => {
         }, 3000);
     }
 
-    // Agregamos la función de validar
-    user.addEventListener('blur', validarCampos);
-    passwd.addEventListener('blur', validarCampos);
-
     // Envio del formulario
     formulario.addEventListener('submit', envioFormulario);
 
@@ -120,14 +118,10 @@ document.addEventListener('DOMContentLoaded', e => {
                 text: `¡El campo de ${e.target.id} es obligatorio!`,
                 footer: '<a href="https://soporte.unidrogas.co/zoho/" target="_blank">¿Tienes un problema?</a>'
             });
-            // Reseteamos el valor del objeto
-            datosMultimedia.deposit = '';
-            // Validamos el objeto
-            validarObjeto();
             return;
         }
         datosMultimedia.deposit = e.target.value;
-        validarObjeto();
+        validacionForm.deposit = true;
     });
 
     // Agregamos el evento para cuando suban un archivo
@@ -142,23 +136,13 @@ document.addEventListener('DOMContentLoaded', e => {
                 text: "Extensión de archivo invalida",
                 footer: '<a href="https://soporte.unidrogas.co/zoho/" target="_blank">¿Tienes un problema?</a>'
             });
-            // Reseteamos el valor del objeto
-            datosMultimedia.multi = '';
-            // Validamos el objeto
-            validarObjeto();
-            // Resetear el valor del input
             multimedia.value = '';
             icon.textContent = 'Subir archivos';
             return;
         }
         icon.textContent = `Archivo subido: ${nombreArchivo}`;
         datosMultimedia.multi = e.target.value;
-        validarObjeto();
+        validacionForm.multi = true;
     });
-
-    // repCaptcha.addEventListener('click', e => {
-    //     console.log(e.target.value);
-    // });
-
 });
 
